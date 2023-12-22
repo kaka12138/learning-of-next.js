1ccfc4f76dbc60b4023c2ee0b3c53a5726635bcb
chenjie <kaka12138@qq.com>
2023-12-22 16:21:48 +0800

    feat: custom ele and handle img in markdow

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,26 @@
 import type { MDXComponents } from "mdx/types" 
+import Image from "next/image"
+function genImg(props: any) {
+  let { src, alt, title } = props
+  let { width, height }= JSON.parse(title)
+  if(!src) src = ""
+  if(!width) width = 200
+  if(!height) height = 200
+
+  return (
+    <Image
+      src={ src }
+      width={ Number(width) }
+      height={ Number(height) }
+      {...props}
+    />
+  )
+}
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    // 自定义元素，修改markdown输出的结果或者样式
+    h1: ({children}) => <h1 style={{ color: "red" }}>{ children }</h1>,
+    img: (props) => genImg(props),
     ...components
   }
 }
