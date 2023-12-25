dc4ed8ee47990cb8e9670f71610e21ec2bda6e29
chenjie <kaka12138@qq.com>
2023-12-25 17:21:46 +0800

    feat: metadata by based-filed(icon)

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,8 +1,10 @@
+import { cookies } from "next/headers"
 interface IRouterInfo {
   params: {
     slug: string
   }
 }
 export default function Page({ params }: IRouterInfo) {
+  cookies().getAll()
   return <h2>My Post: { params.slug }</h2>
 }
